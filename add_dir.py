7189ca3078ad052c8aa4288099fc592dc104c6df
z3r0x0N3 <z3r0x0N3@users.noreply.github.com>
2025-10-27 06:16:04 -0500

    Auto backup: 2025-10-27 06:16:04

--- add_dir.py
+++ add_dir.py
@@ -179,7 +179,7 @@ def get_service_status(service_name):
 
 # Register some globals in a controlled way
 def globalize():
-    import subprocess
+    import su
     global sub, variable, alias
     sub = subprocess
     variable = []
