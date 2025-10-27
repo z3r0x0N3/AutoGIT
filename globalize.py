65bd16a2c47ba625bcb16cf44fc5278aefc54919
z3r0x0N3 <z3r0x0N3@users.noreply.github.com>
2025-10-27 00:57:01 -0500

    Auto backup: 2025-10-27 00:57:01

--- globalize.py
+++ globalize.py
@@ -1,8 +1,18 @@
-def globalize("variable': ' {} ,"alias': ' {} ",):
+def globalize("variable': {}' ,"alias': {}' ,):
     variable = []
     alias = []
     sub = subprocess 
+    global variable
+    global alias 
     global sub  
     print(sub)
 
-globalize()
+
+def globals():
+    globals = for variable and alias in globalize()
+    print(globals)
+
+def show_globals():
+    print
+    print(sub)
+
